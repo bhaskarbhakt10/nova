4ce7e2ea3c6de501f324f7a9ec59d837f87bf47b
Bhaskar Bhakt <bhaskarbhakt10@gmail.com>
2022-06-18 13:34:58 +0530

    setup complete

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>NUIT NOVA</title>
+    <?php require_once('asset/php-sections/css.php') ;?>
+</head>
+<body>
+    
+
+
+
+
+
+<footer>
+    <?php require_once('asset/php-sections/js.php') ?>
+</footer>
+</body>
+</html>
